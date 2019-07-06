ab99a848c2c34f14c7b79c2021286f8f94e44501
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2019-07-06 20:43:40 +0900

    nodejsのサーバのサンプルを追加

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var http = require('http');
+let http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World');
 }).listen(8080, '127.0.0.1');
