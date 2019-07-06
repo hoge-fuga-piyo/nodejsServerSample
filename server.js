4776d92658a58213624f0897cb29850c7d828e62
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2019-07-06 20:30:24 +0900

    nodejsのサーバのサンプルを追加

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-// httpモジュールを読み込み、インスタンスを生成
 var http = require('http');
 
-// HTTPサーバーのイベントハンドラを定義
 http.createServer(function (req, res) {
-
-  // HTTPヘッダを出力
   res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  // レスポンスの中身を出力
   res.end('Hello World\n');
-
 }).listen(8080, '127.0.0.1');
